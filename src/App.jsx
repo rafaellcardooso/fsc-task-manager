740ca15e2569a316da245ef3c3889abafea51fab
Rafael Cardoso <r.cardoso.10@hotmail.com>
2025-12-27 18:52:23 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Tasks from "./components/Tasks";
+
+function App() {
+  return <h1>FSC</h1>;
+}
+
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import Header from "./Header";
+
+function Tasks() {}
+
+export default Tasks;
