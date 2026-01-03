f7016f9a9c87f769119f705354121409c0be1de8
Rafael Cardoso <r.cardoso.10@hotmail.com>
2026-01-03 17:40:11 -0300

    feat: add Button components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ const Button = ({ children, variant = "primary" }) => {
 
   return (
     <button
-      className={`flex items-center gap-2 rounded-md px-3 py-1 text-xs font-semibold ${getVariantClasses()}`}
+      className={`flex items-center gap-2 rounded-md px-3 py-1 text-xs font-semibold transition hover:opacity-75 ${getVariantClasses()}`}
     >
       {children}
     </button>
