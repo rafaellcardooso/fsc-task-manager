c780727f349493b72d6d7c7e6424b68eaee04a9b
Rafael Cardoso <r.cardoso.10@hotmail.com>
2026-05-01 16:58:40 -0300

    perf: ensure inputs are uncontrolled on AddTaskDialog

--- src/components/TimeSelect.jsx
+++ src/components/TimeSelect.jsx
@@ -1,6 +1,8 @@
+import { forwardRef } from "react"
+
 import InputLabel from "./InputLabel"
 
-const TimeSelect = (props) => {
+const TimeSelect = forwardRef((props, ref) => {
   return (
     <div className="flex flex-col gap-1 text-left">
       <InputLabel htmlFor="time">Horário</InputLabel>
@@ -10,6 +12,7 @@ const TimeSelect = (props) => {
         className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[#9A9C9F] focus:outline"
         value="morning"
         {...props}
+        ref={ref}
       >
         <option value="morning">Manhã</option>
         <option value="afternoon">Tarde</option>
@@ -21,6 +24,8 @@ const TimeSelect = (props) => {
       )}
     </div>
   )
-}
+})
+
+TimeSelect.displayName = "TimeSelect"
 
 export default TimeSelect
