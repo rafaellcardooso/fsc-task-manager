84c33d1ef81c5303a3302d55201305a6d052fbe7
Rafael Cardoso <r.cardoso.10@hotmail.com>
2026-05-01 14:52:14 -0300

    fix: corrigir importação do ícone de tarefas no Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { HomeIcon, TaskIcon } from "../assets/icons"
+import { HomeIcon, TasksIcon } from "../assets/icons"
 import SidebarButton from "./SidebarButton"
 
 const Sidebar = () => {
@@ -18,7 +18,7 @@ const Sidebar = () => {
           Início
         </SidebarButton>
         <SidebarButton variant="selected">
-          <TaskIcon />
+          <TasksIcon />
           Minhas Tarefas
         </SidebarButton>
       </div>
