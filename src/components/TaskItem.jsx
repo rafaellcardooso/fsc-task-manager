273d7fe4948bc0653058aa2fe0700adab8e4f580
Rafael Cardoso <r.cardoso.10@hotmail.com>
2026-07-11 21:02:02 -0300

    feat: add react router dom and initial TaskDetails pages

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types"
 import { useState } from "react"
+import { Link } from "react-router-dom"
 import { toast } from "sonner"
 
 import { CheckIcon, DetailsIcon, LoaderIcon, TrashIcon } from "../assets/icons"
@@ -71,9 +72,9 @@ const TaskItem = ({ task, handleCheckboxClick, onDeleteSuccess }) => {
           )}
         </Button>
 
-        <a href="#" className="transition hover:opacity-75">
+        <Link to={`/task/${task.id}`}>
           <DetailsIcon />
-        </a>
+        </Link>
       </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,25 @@
 import "./index.css"
 
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 import App from "./App.jsx"
+import TaskDetailsPage from "./pages/task-details.jsx"
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/task/:taskId",
+    element: <TaskDetailsPage />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
